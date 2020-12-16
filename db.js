fae7789a758d59f2a7b339cfbd2dcab3aa9a8d03
Tobias Schoon <tobias-schoon@web.de>
2020-12-16 13:41:55 +0100

    db.js added, group.html start

---
+++ db.js
@@ -0,0 +1,9 @@
+// fake db
+var groups = [
+    {name: "Lineare Algebra 2", description: "interessante Beschreibung", participants: "Emil, Peter Z.", dates: "Mo, 09:00 Uhr - 10:00 Uhr"},
+    {name: "Lineare Algebra 3", description: "interessante Beschreibung", participants: "Emil, Peter Z.", dates: "Di, 09:00 Uhr - 10:00 Uhr"}
+];
+
+function getGroups(user) {
+    return groups;
+}
