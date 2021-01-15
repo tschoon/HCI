5ab9365dbee5eccf3ea63bc84b79fe3f8150ddae
Constantin Tremel <constantin.tremel@web.de>
2021-01-15 13:19:19 +0100

    polishing

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 // fake db
 var groups = [
-    {name: "Lineare Algebra 2", description: "interessante Beschreibung", participants: "Emil, Peter Z.", dates: [[new Date("2024-01-01T08:10:00Z"), new Date("2024-01-01T10:10:00Z")], [new Date("2024-01-03T08:10:00Z"), new Date("2024-01-03T10:10:00Z")]]},
+    {name: "Lineare Algebra 2", description: "interessante Beschreibung", participants: "Emil, Peter Z.", dates: [[new Date("2024-01-03T13:10:00Z"), new Date("2024-01-03T13:55:00Z")], [new Date("2024-01-05T06:10:00Z"), new Date("2024-01-05T07:40:00Z")]]},
     {name: "Lineare Algebra 3", description: "interessante Beschreibung", participants: "Emil, Peter Z.", dates: [[new Date("2024-01-02T08:10:00Z"), new Date("2024-01-02T10:10:00Z")]]},
 	{name: "Programming?", description: "C, C++, Java, Python 3"},
 	{name: "Sightseeing in Prague", description: "we go to Prague, ocassionally"},
